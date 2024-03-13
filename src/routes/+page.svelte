14f48ded5263f9574f87f51415dcf562a407fb95
Ben Yang <beyang@adobe.com>
2024-03-12 18:24:13 -0700

    trying mousedonw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -427,9 +427,8 @@
 	<div>
 		{#each Object.values(tonesTemperment) as t}
 			<button
-				on:touchstart={() => (tones[t].playing = true)}
-				on:touchend={() => (tones[t].playing = false)}
-				on:touchcancel={() => (tones[t].playing = false)}>{tones[t].name}{tones[t].octave}</button
+				on:mousedown={() => (tones[t].playing = true)}
+				on:mouseup={() => (tones[t].playing = false)}>{tones[t].name}{tones[t].octave}</button
 			>
 		{/each}
 	</div>
